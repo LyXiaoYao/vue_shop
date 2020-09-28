5302a9ac92e00196a3fa449d05086c0e720baf15
light <15111009275@163.com>
2020-09-28 19:48:53 +0800

    完成了登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+// 导入弹框提示组件Message
 
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+    //把弹框组件挂在到Vue实例对象上
+Vue.prototype.$message = Message
