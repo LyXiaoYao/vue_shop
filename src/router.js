962153c7fb1ff231c81b73139bed649711fcd8a1
light <15111009275@163.com>
2020-10-04 15:06:36 +0800

    商品功能

--- src/router.js
+++ src/router.js
@@ -8,6 +8,8 @@ import Rights from './components/power/Rights.vue'
 import Roles from './components/power/Roles.vue'
 import Cate from './components/goods/Cate.vue'
 import Params from './components/goods/Params.vue'
+import GoodsList from './components/goods/List.vue'
+import AddGoods from './components/goods/Add.vue'
 
 Vue.use(Router)
 
@@ -47,7 +49,15 @@ const router = new Router({
                 {
                     path: '/params',
                     component: Params
+                }, {
+                    path: '/goods',
+                    component: GoodsList,
+                },
+                {
+                    path: '/goods/add',
+                    component: AddGoods,
                 }
+
             ]
         }
     ]
